471a9ad32c5aadadf67e437a43c7e37f6ea0ee69
dukaludic <73173776+dukaludic@users.noreply.github.com>
2021-06-20 20:26:30 +0200

    new password

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { Redirect, Route, Switch } from "react-router-dom";
+import "./App.css";
+import MainNav from "./components/MainNav";
+import PasswordItem from "./components/PasswordItem";
+import NewPassword from "./components/NewPassword";
 
 function App() {
+  const DUMMY_LOGINS = [
+    {
+      title: "Facebook",
+      username: "hello123",
+      password: "asdf1141",
+    },
+    {
+      title: "Twitter",
+      username: "world123",
+      password: "hellouu12",
+    },
+    {
+      title: "Instagram",
+      username: "doodle15",
+      password: "helloworld11",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MainNav />
+      <Switch>
+        <Route path="/" exact>
+          <p>Log in</p>
+        </Route>
+        <Route path="/profile"></Route>
+        <Route path="/password">
+          <PasswordItem title={DUMMY_LOGINS[0].title} />
+          <PasswordItem title={DUMMY_LOGINS[1].title} />
+          <PasswordItem title={DUMMY_LOGINS[2].title} />
+          <NewPassword />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/NewPassword.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+
+const NewPassword = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredPw, setEnteredPw] = useState("");
+
+  const clickHandler = () => {
+    isOpen === true ? setIsOpen(false) : setIsOpen(true);
+  };
+
+  const titleChangeHandler = (e) => {
+    setEnteredTitle(e.target.value);
+  };
+
+  const pwChangeHandler = (e) => {
+    setEnteredPw(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      password: enteredPw,
+    };
+
+    console.log(expenseData);
+    setEnteredPw("");
+    setEnteredTitle("");
+  };
+
+  return (
+    <div className="newPasswordContainer">
+      {!isOpen && (
+        <button className="addBtn" onClick={clickHandler}>
+          ADD
+        </button>
+      )}
+      {isOpen && (
+        <div>
+          <div className="pwItemContainer">
+            <form onSubmit={submitHandler}>
+              <div>
+                <div>
+                  <label>Title</label>
+                  <input
+                    type="text"
+                    value={enteredTitle}
+                    onChange={titleChangeHandler}
+                  />
+                </div>
+                <div>
+                  <label>Password</label>
+                  <input
+                    type="text"
+                    value={enteredPw}
+                    onChange={pwChangeHandler}
+                  />
+                </div>
+              </div>
+              <div>
+                <button>Add</button>
+              </div>
+            </form>
+            {isOpen && (
+              <button className="addBtn" onClick={clickHandler}>
+                CLOSE
+              </button>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default NewPassword;
